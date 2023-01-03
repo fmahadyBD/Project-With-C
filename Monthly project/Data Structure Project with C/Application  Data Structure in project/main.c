20e21a77ef9a527b2f6e4ce08b4744fe8b840481
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-03 17:47:08 +0600

    Update main.c

--- Monthly project/Data Structure Project with C/Application  Data Structure in project/main.c
+++ Monthly project/Data Structure Project with C/Application  Data Structure in project/main.c
@@ -29,6 +29,108 @@ int login()
     }
 }
 
+void enqueue(){
+   int num;
+   printf("Enter the number to be inserted: ");
+   scanf("%d", &num);
+
+
+   if(rear == MAX-1){
+      printf("Overflow");
+   }
+
+   else if(front == -1 && rear == -1){
+      front = 0;
+      rear = 0;
+   }
+  
+   else if(rear == MAX - 1 && front != 0){
+      rear = 0;
+   }
+   else {
+      rear += 1;
+   }
+
+   circQueue[rear] = num;
+}
+
+
+void dequeue(){
+
+   if(front == -1){
+      printf("noone in line");
+   }
+
+   else if(front == rear){
+      front = -1;
+      rear = -1;
+   }
+
+   else if(front == MAX - 1){
+      front = 0;
+   }
+   else {
+      front += 1;
+   }
+}
+
+void displays(){
+    int i;
+    if(front == -1 || rear == -1)
+    {
+        printf("\nEmpty line\n");
+    }
+    else {
+       printf("The line is: ");
+       for (i = front; i <= rear; i++){
+       printf("%d ", circQueue[i]);
+    }
+    printf("\n");
+  }
+}
+
+int Qeue()
+{
+  printf("Welcome to ticket manage ment systtem:\n");
+    int choice;
+    while(1){
+        printf("Enter your choice: \n1. Join n line forticket \n");
+        printf("\n2.Ticket provide:\n");
+        printf("\n3. Display the line of numbers are wating forticket\n4. Exit\n");
+        printf("4. Exit\n");
+        scanf("%d", &choice);
+
+    switch(choice){
+       case 1:
+          enqueue(); 
+          break;
+       case 2:
+          dequeue(); 
+          break;
+       case 3:
+          displays(); 
+          break;
+       case 4:
+          return 0;  
+       default:
+          printf("Enter valid choice");
+          break;
+       }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int Sorting()
 
 {
